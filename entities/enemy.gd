f149b70f7072aa781fe600a44d9257b0b7d1dd81
Moustache9593 <citybuilderdeveloper@proton.me>
2024-06-17 20:42:53 -0600

    Added gui elements.

---
+++ entities/enemy.gd
@@ -0,0 +1,10 @@
+extends TextureRect
+
+@export var MAX_HEALTH = 40.0
+@export var ATTACK = 20.0
+@onready var health = MAX_HEALTH
+
+
+func _ready():
+	pass
+
---
+++ entities/player.gd
@@ -0,0 +1,5 @@
+extends Node
+var health = 100.0
+var strength = 60.0
+
+
